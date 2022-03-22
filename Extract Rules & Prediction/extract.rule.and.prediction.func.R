fe5572c890b69182be85ece849a47fd2759bdac7
Ruijie Yin <ruijieyin428@gmail.com>
2022-03-22 18:33:30 -0400

    Update extract.rule.and.prediction.func.R

--- Extract Rules & Prediction/extract.rule.and.prediction.func.R
+++ Extract Rules & Prediction/extract.rule.and.prediction.func.R
@@ -179,4 +179,29 @@ prediction.results <- data.frame("sample no." = 1:nrow(test_input),
                                  "predicted class labels" = final.decisions[,1])
 #return(prediction.results)
 
+# see performance: Cohen's Kappa
+# 1st row: reference labels
+# 2nd row: predicted labels
+prediction <- matrix(prediction.results[,2], nrow = 1)
+
+compare_labels <- rbind(matrix(test_input[,dim(test_input)[2]], nrow = 1), 
+                        prediction)
+
+remove <- which(prediction[1,] == "UNS")
+
+if (length(remove) == 0) {
+  compare_labels <- compare_labels
+} else if (length(remove) != 0) {
+  compare_labels <- compare_labels[,-remove]
+}
+
+# Concordance: kappa statistic
+# will need a matrix where the diagonal elements of the matrix are 
+# the agreeing elements; the discordant observations are on the off-diagonal.
+# A confusion matrix:
+con_table <- confusionMatrix(data = as.factor(compare_labels[2,]),
+                           reference = as.factor(compare_labels[1,]))
+con_table$table
+con_table$overall
+
 
