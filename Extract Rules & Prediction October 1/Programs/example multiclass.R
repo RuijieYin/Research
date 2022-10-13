e43c41a6d70c0bbb361549ad7816d3eeeba4068b
Ruijie Yin <ruijieyin428@gmail.com>
2022-10-13 14:39:24 -0400

    Add files via upload

---
+++ Extract Rules & Prediction October 1/Programs/example multiclass.R
@@ -0,0 +1,36 @@
+# load treeRule.Obtain.Judge.R
+source("treeRule_Obtain_Judge.R")
+# load split.extract.predict.R
+source("split_extract_predict_multiclass_balanced_trees_1v1_ver5.R")
+
+
+# split the training data (TCGA BRCA 1095 samples: 800 training, 200 validation, 95 test)
+data.split <- split.train(data = read.csv(file = "tcga_quantiles_50.csv", header = T, row.names = 1))
+# continue to split the training data obtained from function 1 using a 1-vs-rest scheme
+training_data <- split_one_vs_one_data_train(data = data.split$train.data)
+
+# extract rules:
+rules <- extract.rules(data = training_data, n.trees = 20, mtry = 5, node_depth_max = 5)
+# see all rules extracted from the n.tree trees
+# rules$Rules
+write.csv(rules$Rules, "rules_extracted_1v1_ver2.csv", row.names = F)
+
+
+gc()
+# use validation data to select the optimal number of rules for each subclass
+selected.rules <- select.rules(validation.data = data.split$validation.data, rule.table = rules$Rules)
+# # see the optimal rules selected for each subclass
+write.csv(selected.rules$Selected.Rules, "multiclass1_rules_1v1_ver4.csv", row.names = F)
+
+
+# Predict on the test data (the remaining 95 samples)
+pred <- predict.rules(data = data.split$test.data, decision.rules = final.decision.rules.untrim)
+# see the predicted class labels for the test data:
+# pred$Prediction.results
+# see prediction accuracy and cohen's kappa:
+print(c("n.trees" = 20, "mtry" = 5,
+        "node_depth_max" = 5,
+        pred$Results.table))
+
+
+
